e8ed5dae7f0de5185d26e605300973315ea81d69
Ricardo Galdámez <ricardo.j.galdamez@gmail.com>
2016-03-24 20:01:27 -0600

    Reempezando Insertar

--- src/arbolbjava/Tree.java
+++ src/arbolbjava/Tree.java
@@ -24,17 +24,63 @@ public class Tree {
         this.root = root;
     }
     
-    private void insert(int key, TreeNode Node){
-        if (Node.isLeaf() && !Node.isFull()){
-            if(Node.leftEmpty()){
-                Node.setLeftKey(key);
-            }else if(!Node.leftEmpty() && Node.rightEmpty()){
-                Node.setRightKey(key);
+    public void insert(int key, TreeNode Node){
+        if (Node.getFather() == null && Node.isLeaf()){
+            if (!Node.isFull()){
+                if (Node.leftEmpty() && Node.rightEmpty()){
+                    Node.setLeftKey(key);
+                }else if (!Node.leftEmpty() && Node.rightEmpty()){
+                    Node.setRightKey(key);
+                }
+                order(Node);
+            }else{
+                int key1 = Node.getLeftKey();
+                int key2 = Node.getRightKey();
+                int key3 = key;
+                TreeNode leftTemp, rightTemp;
+                
+                if (key3 < key1){
+                    
+                    leftTemp = new TreeNode(key3,Node);
+                    rightTemp = new TreeNode(key2,Node);
+                    Node = new TreeNode(key1);
+                    leftTemp.setFather(Node);
+                    rightTemp.setFather(Node);
+                    Node.setLeftBranch(leftTemp);
+                    Node.setRightBranch(rightTemp);             
+                    
+                }else if (key3 > key2){
+                    
+                    leftTemp = new TreeNode(key1,Node);
+                    rightTemp = new TreeNode(key,Node);
+                    Node = new TreeNode(key2);
+                    leftTemp.setFather(Node);
+                    rightTemp.setFather(Node);
+                    Node.setLeftBranch(leftTemp);
+                    Node.setRightBranch(rightTemp);
+                }else {
+                    
+                    leftTemp = new TreeNode(key1,Node);
+                    rightTemp = new TreeNode(key2,Node);
+                    Node = new TreeNode(key);
+                    leftTemp.setFather(Node);
+                    rightTemp.setFather(Node);
+                    Node.setLeftBranch(leftTemp);
+                    Node.setRightBranch(rightTemp);
+                }
+               
             }
-        }else{
-            
         }
+        
     }
     
-    
+    public void order(TreeNode node){
+        int left = node.getLeftKey();
+        int right = node.getRightKey();
+        if (left > right){
+            node.setLeftKey(right);
+            node.setRightKey(left);
+        }
+    }
+ 
 }
--- src/arbolbjava/TreeNode.java
+++ src/arbolbjava/TreeNode.java
@@ -10,11 +10,13 @@ package arbolbjava;
  * @author RickAg
  */
 public class TreeNode {
+
     private int leftKey;
     private int rightKey;
     private TreeNode leftBranch;
     private TreeNode middleBranch;
     private TreeNode rightBranch;
+    private TreeNode father;
 
     public TreeNode() {
         leftKey = -1;
@@ -22,8 +24,45 @@ public class TreeNode {
         leftBranch = null;
         rightBranch = null;
         middleBranch = null;
+        father = null;
     }
-    
+
+    public TreeNode(TreeNode father) {
+        leftKey = -1;
+        rightKey = -1;
+        leftBranch = null;
+        rightBranch = null;
+        middleBranch = null;
+        father = father;
+    }
+
+    public TreeNode(int leftKey, int rightKey, TreeNode father) {
+        this.leftKey = leftKey;
+        this.rightKey = rightKey;
+        this.father = father;
+        leftBranch = null;
+        rightBranch = null;
+        middleBranch = null;
+    }
+
+    public TreeNode(int leftKey, TreeNode father) {
+        this.leftKey = leftKey;
+        this.rightKey = -1;
+        this.father = father;
+        leftBranch = null;
+        rightBranch = null;
+        middleBranch = null;
+    }
+
+    public TreeNode(int leftKey) {
+        this.leftKey = leftKey;
+        rightKey = -1;
+        leftBranch = null;
+        rightBranch = null;
+        middleBranch = null;
+        father = null;
+    }
+   
     public int getLeftKey() {
         return leftKey;
     }
@@ -63,39 +102,46 @@ public class TreeNode {
     public void setRightBranch(TreeNode rightBranch) {
         this.rightBranch = rightBranch;
     }
-    
-    boolean isLeaf(){
+
+    public TreeNode getFather() {
+        return father;
+    }
+
+    public void setFather(TreeNode father) {
+        this.father = father;
+    }
+
+    boolean isLeaf() {
 //        if (leftBranch.getLeftBranch() == null && leftBranch.getMiddleBranch() == null && leftBranch.getRightBranch() == null &&
 //                middleBranch.getLeftBranch() == null && middleBranch.getMiddleBranch() == null && middleBranch.getRightBranch() == null &&
 //                rightBranch.getLeftBranch() == null && rightBranch.getMiddleBranch() == null && rightBranch.getRightBranch() == null){
 //            return true;
 //        }
-        if (leftBranch == null && middleBranch == null && rightBranch == null){
+        if (leftBranch == null && middleBranch == null && rightBranch == null) {
             return true;
         }
         return false;
     }
-    
-    boolean isFull(){
-        if (leftKey != -1 && rightKey !=-1){
+
+    boolean isFull() {
+        if (leftKey != -1 && rightKey != -1) {
             return true;
         }
         return false;
     }
-    
-    boolean leftEmpty(){
-        if (leftKey !=-1){
+
+    boolean leftEmpty() {
+        if (leftKey != -1) {
             return false;
         }
         return true;
     }
-    
-    boolean rightEmpty(){
-        if (rightKey !=-1){
+
+    boolean rightEmpty() {
+        if (rightKey != -1) {
             return false;
         }
         return true;
     }
-   
-    
+
 }
