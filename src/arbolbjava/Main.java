cca989913e310418811a40daca9de04ccc22a3d3
Ricardo Galdámez <ricardo.j.galdamez@gmail.com>
2016-03-28 06:47:26 -0600

    ...main

--- src/arbolbjava/Main.java
+++ src/arbolbjava/Main.java
@@ -5,6 +5,8 @@
  */
 package arbolbjava;
 
+import java.util.Scanner;
+
 /**
  *
  * @author RickAg
@@ -16,14 +18,44 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        Tree arbol = new Tree(new TreeNode(10));
-        arbol.insert(20, arbol.getRoot());
-        arbol.insert(30, arbol.getRoot());
-        arbol.insert(40, arbol.getRoot());
-        arbol.insert(50, arbol.getRoot());
-        
-        arbol.print(arbol.getRoot(), 1);
+        Scanner sc = new Scanner(System.in);
+        int opcion = -1;
+        Tree tree = new Tree(new TreeNode());
+        do {
+            opcion = menu();
+
+            if(opcion == 1){
+                System.out.println("Que llave desea agregar?");
+                int llave = sc.nextInt();
+                tree.insert(llave, tree.getRoot());
+                System.out.println("Llave agregada");
+
+            }else if (opcion ==2 ){
+                tree.print(tree.getRoot(), 1);
+
+            }else if (opcion ==3 ){
+                tree.saveTree();
+                System.out.println("Arbol guardado");
+
+            }else if (opcion ==4){
+                tree.loadTree();
+                System.out.println("Arbol cargado");
+            }
+        }while (opcion != 5);
+    }
+    
+    public static int menu(){
+        Scanner sc = new Scanner(System.in);
+        int seleccion = -1;
+        while (seleccion > 5 || seleccion < 1){
+            System.out.println("1) Agregar en el arbol");
+            System.out.println("2) Imprimir Arbol");
+            System.out.println("3) Salvar Arbol");
+            System.out.println("4) Cargar arbol");
+            System.out.println("5) Salir");
+            seleccion = sc.nextInt();
+        }
+        return seleccion;
     }
     
 }
